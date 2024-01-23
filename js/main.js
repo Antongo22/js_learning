c398e77fa2335b34bbf2ea81439c2c56a1cbd585
Anton <80984465+Antongo22@users.noreply.github.com>
2024-01-23 12:11:44 +0300

    Заданиче на паре

--- js/main.js
+++ js/main.js
@@ -1,122 +1,77 @@
- 
-//1
-/* alert("Добрый день!"); */
+/* let x = parseInt(prompt("Введите число:"));
 
-//2
-/* let name = prompt("Введите своё имя:");
-alert("Привет, " + name + "! Добро пожаловать   !");
- */
-//3
-/* 
-let isAgeConfirmed = confirm("Вам есть 18 лет?");
-if(isAgeConfirmed){
-    alert("Пользователь подтвердил свой возраст" );
-}
-else{
-    alert("Пользователь не подтвердил свой возраст");
-}
+let result = x % 2 === 0 ? "Четное число" : "Нечетное число";
 
-  */
+alert(result); */
 
-//4
-/* let number1 = parseFloat(prompt("Введите первое число:"));
-let number2 = parseFloat(prompt("Введите второе число:"));
-let sum = number1 + number2;
-alert("Сумма двух чисел: " + sum);
- */
-//5
-/* let variable1 = "1";
-let variable2 = "2";
+/* let a = parseFloat(prompt("Введите число a:"));
+let b = parseFloat(prompt("Введите число b:"));
 
-alert("Первая переменная: " + variable1 + "\nВторая переменная: " + variable2);
+let result = a < b ? a : b;
 
-let temp = variable1;
-variable1 = variable2;
-variable2 = temp;
+alert("Меньшее из чисел a и b: " + result); */
 
-alert("Первая переменная: " + variable1 + "\nВторая переменная: " + variable2);  
- */
-//6
-/* let length = parseFloat(prompt("Введите длину прямоугольника:"));
-let width = parseFloat(prompt("Введите ширину прямоугольника:"));
+/* let number = parseFloat(prompt("Введите число:"));
 
-let area = length * width;
-alert("Площадь прямоугольника: " + area);
- */
+let result = number > 0 ? "Число положительное" : number < 0 ? "Число отрицательное" : "Число равно нулю";
 
-//7
+alert(result); */
 
-/* let number1 = parseFloat(prompt("Введите первое число:"));
-let number2 = parseFloat(prompt("Введите второе число:"));
-
-let sum = number1 + number2;
-let difference = number1 - number2;
-let product = number1 * number2;
-let quotient = number1 / number2;
-
-alert("Сумма: " + sum + "\nРазница: " + difference + "\nПроизведение: " + product + "\nЧастное: " + quotient); */
-
-//8 
-/* let number = parseInt(prompt("Введите число:"));
-
-if (number % 2 === 0) {
-  alert(number + " - четное число");
-} else {
-  alert(number + " - нечетное число");
-} */
-
-//9
 /* let age = parseInt(prompt("Введите ваш возраст:"));
 
-if (age >= 18) {
-  alert("Вы можете принять участие в акции со скидкой!");
-} else {
-  alert("К сожалению, вы не можете принять участие в акции со скидкой.");
-} */
+let result = age >= 18 ? "Взрослый" : "Не взрослый";
 
-// 10
-/* let length = parseInt(prompt("Введите длину пароля:"));
+alert("Ваш статус: " + result); */
 
-let password = generatePassword(length);
-alert("Сгенерированный пароль: " + password);
+/* 
+let currentHour = new Date().getHours();
 
-function generatePassword(length) {
-  let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  let password = "";
+let timeOfDay =
+  currentHour >= 6 && currentHour < 12 ? "утро" :
+  currentHour >= 12 && currentHour < 18 ? "день" :
+  currentHour >= 18 && currentHour < 24 ? "вечер" :
+  "ночь";
 
-  for (let i = 0; i < length; i++) {
-    let randomIndex = Math.floor(Math.random() * charset.length);
-    password += charset[randomIndex];
-  }
+alert("Сейчас " + timeOfDay); */
 
-  return password;
-} */
 
-// 11
-/* let number1 = parseInt(prompt("Введите первое число:"));
-let number2 = parseInt(prompt("Введите второе число:"));
+/* let number = parseInt(prompt("Введите число:"));
 
-function findGCD(a, b) {
-  if (b === 0) {
-    return a;
-  } else {
-    return findGCD(b, a % b);
+let result = number % 5 === 0 ? "Число делится на 5 без остатка" : "Число не делится на 5 без остатка";
+
+alert(result); */
+
+
+/* let word = prompt("Введите слово:");
+
+let result = word.length < 5 ? "короткое" : "длинное";
+
+alert("Это слово - " + result); */
+
+
+/* let a = parseFloat(prompt("Введите число a:"));
+let b = parseFloat(prompt("Введите число b:"));
+
+let result = a === b ? "равны" : "разные";
+
+alert("Числа a и b " + result); */
+
+
+let number = parseInt(prompt("Введите число:"));
+let isPrime = true;
+
+if (number === 1) {
+  isPrime = false;
+} else if (number > 1) {
+  for (let i = 2; i < number; i++) {
+    if (number % i === 0) {
+      isPrime = false;
+      break;
+    }
   }
 }
 
-let gcd = findGCD(number1, number2);
-alert("Наибольший общий делитель: " + gcd); */
+let result = isPrime ? "простое" : "составное";
+alert("Число " + number + " - " + result);
 
-//12
 
-let string = prompt("Введите строку:");
-
-let processedString = string.replace(/\s/g, "").toLowerCase();
-
-let reversedString = processedString.split("").reverse().join("");
-
-if (processedString === reversedString) {
-  alert("Введенная строка является палиндромом");
-} else {
-  alert("Введенная строка не является палиндромом");
-}
