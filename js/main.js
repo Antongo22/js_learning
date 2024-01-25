714d14c01024ddcae99ce824cd6603a7b73736ad
Anton <80984465+Antongo22@users.noreply.github.com>
2024-01-25 09:54:10 +0300

    Работа на паре

--- js/main.js
+++ js/main.js
@@ -1,77 +1,30 @@
-/* let x = parseInt(prompt("Введите число:"));
-
-let result = x % 2 === 0 ? "Четное число" : "Нечетное число";
-
-alert(result); */
-
-/* let a = parseFloat(prompt("Введите число a:"));
-let b = parseFloat(prompt("Введите число b:"));
-
-let result = a < b ? a : b;
-
-alert("Меньшее из чисел a и b: " + result); */
-
-/* let number = parseFloat(prompt("Введите число:"));
-
-let result = number > 0 ? "Число положительное" : number < 0 ? "Число отрицательное" : "Число равно нулю";
-
-alert(result); */
-
-/* let age = parseInt(prompt("Введите ваш возраст:"));
-
-let result = age >= 18 ? "Взрослый" : "Не взрослый";
-
-alert("Ваш статус: " + result); */
-
+// do while провера на полиндром
 /* 
-let currentHour = new Date().getHours();
+let word = prompt('Введите слово:');
+let reversedWord = '';
+let i = word.length - 1;
 
-let timeOfDay =
-  currentHour >= 6 && currentHour < 12 ? "утро" :
-  currentHour >= 12 && currentHour < 18 ? "день" :
-  currentHour >= 18 && currentHour < 24 ? "вечер" :
-  "ночь";
+do {
+  reversedWord += word[i];
+  i--;
+} while (i >= 0);
 
-alert("Сейчас " + timeOfDay); */
+let result = word.toLowerCase() === reversedWord.toLowerCase() ? 'является полиндромом' : 'не является полиндромом';
 
+alert(`Слово "${word}" ${result}`);
+ */
 
-/* let number = parseInt(prompt("Введите число:"));
+// do while проверка 
 
-let result = number % 5 === 0 ? "Число делится на 5 без остатка" : "Число не делится на 5 без остатка";
+let input = prompt('Введите числа через запятую:');
+let numbers = input.split(',').map(num => parseInt(num.trim()));
 
-alert(result); */
+let sum = 0;
+let i = 0;
 
+do {
+  sum += numbers[i];
+  i++;
+} while (i < numbers.length);
 
-/* let word = prompt("Введите слово:");
-
-let result = word.length < 5 ? "короткое" : "длинное";
-
-alert("Это слово - " + result); */
-
-
-/* let a = parseFloat(prompt("Введите число a:"));
-let b = parseFloat(prompt("Введите число b:"));
-
-let result = a === b ? "равны" : "разные";
-
-alert("Числа a и b " + result); */
-
-
-let number = parseInt(prompt("Введите число:"));
-let isPrime = true;
-
-if (number === 1) {
-  isPrime = false;
-} else if (number > 1) {
-  for (let i = 2; i < number; i++) {
-    if (number % i === 0) {
-      isPrime = false;
-      break;
-    }
-  }
-}
-
-let result = isPrime ? "простое" : "составное";
-alert("Число " + number + " - " + result);
-
-
+alert(`Сумма введенных чисел: ${sum}`);
