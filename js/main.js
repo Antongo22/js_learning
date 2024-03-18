9a1313d8bb3ec2050f1cfb30c4672f4b161fd8c7
Anton <80984465+Antongo22@users.noreply.github.com>
2024-03-18 11:01:23 +0300

    задание на паре

--- js/main.js
+++ js/main.js
@@ -1,52 +1,60 @@
-// Создать массив чисел и вывести на экран их сумму
-const numbers = [1, 2, 3, 4, 5];
-const sum = numbers.reduce((acc, curr) => acc + curr, 0);
-console.log("Сумма чисел:", sum);
+// 1. Преобразовать массив чисел в массив квадратов этих чисел - map
+function squareArray(arr) {
+    return arr.map(num => num * num);
+  }
 
-// Создать массив строк и вывести на экран их длины
-const strings = ["apple", "banana", "orange"];
-const lengths = strings.map(str => str.length);
-console.log("Длины строк:", lengths);
+  
+  // 2. Отсортировать массив чисел по возрастанию - sort
+function sortArray(arr) {
+    return arr.sort((a, b) => a - b);
+  }
+  
 
-// Создать массив из чисел и найти среднее значение
-const numArray = [10, 20, 30, 40, 50];
-const average = numArray.reduce((acc, curr) => acc + curr, 0) / numArray.length;
-console.log("Среднее значение чисел:", average);
+  // 3. Перевернуть массив чисел - reverse
+function reverseArray(arr) {
+    return arr.reverse();
+  }
+  
+  
+  // 4. Разделить строку по пробелам и сохранить каждое слово как элемент массива - split / join
+function splitAndJoinString(str) {
+    return str.split(' ');
+  }
+  
+  
+  // 5. Найти сумму элементов в массиве чисел - reduce / reduceRight
+function sumArray(arr) {
+    return arr.reduce((acc, curr) => acc + curr, 0);
+  }
+  
+  
+  // 6. Напишите функцию camelize(str), которая преобразует строки вида my-short-string в myShortString
+function camelize(str) {
+    return str.split('-').map((word, index) => index === 0 ? word : word[0].toUpperCase() + word.slice(1)).join('');
+  }
 
-// Создать массив объектов с информацией о студентах и вывести их на экран
-const students = [
-    { name: "Alice", age: 20 },
-    { name: "Bob", age: 22 },
-    { name: "Charlie", age: 21 }
-];
-console.log("Информация о студентах:", students);
+  // 7. Функция filterRange
+function filterRange(arr, a, b) {
+    return arr.filter(num => num >= a && num <= b);
+  }
+  
 
-// Создать массив и перевернуть его
-const arrayToReverse = [1, 2, 3, 4, 5];
-const reversedArray = arrayToReverse.reverse();
-console.log("Перевернутый массив:", reversedArray);
+  
+  // 8. Функция filterRangeInPlace
+function filterRangeInPlace(arr, a, b) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] < a || arr[i] > b) {
+        arr.splice(i, 1);
+        i--; 
+      }
+    }
+  }
+  
 
-// Создайте пустой массив с переменной myArray
-let myArray = [];
+// 9. Сортировка в порядке убывания
+function sortDescending(arr) {
+    return arr.sort((a, b) => b - a);
+  }
+  
 
-// Добавьте элемент apple в конец массива
-myArray.push("apple");
-
-// Добавьте элемент banana в начало массива
-myArray.unshift("banana");
-
-// Добавьте элемент orange во вторую позицию массива
-myArray.splice(1, 0, "orange");
-
-// Удалите первый элемент массива
-myArray.shift();
-
-// Измените banana на pear
-const bananaIndex = myArray.indexOf("banana");
-if (bananaIndex !== -1) {
-    myArray[bananaIndex] = "pear";
-}
-
-// Выведите в консоль длину массива и его содержимое
-console.log("Длина массива:", myArray.length);
-console.log("Содержимое массива:", myArray);
+  
