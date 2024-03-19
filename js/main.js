860b0be889e254aa04abf76aeff7e3b42c828bcb
Anton <80984465+Antongo22@users.noreply.github.com>
2024-03-19 12:30:00 +0300

    задание на паре

--- js/main.js
+++ js/main.js
@@ -1,60 +1,27 @@
-// 1. Преобразовать массив чисел в массив квадратов этих чисел - map
-function squareArray(arr) {
-    return arr.map(num => num * num);
+function fillArray(value, length) {
+  const newArray = [];
+  for (let i = 0; i < length; i++) {
+    newArray.push(value);
   }
+  return newArray;
+}
 
-  
-  // 2. Отсортировать массив чисел по возрастанию - sort
-function sortArray(arr) {
-    return arr.sort((a, b) => a - b);
-  }
-  
+console.log(fillArray('a', 3)); 
 
-  // 3. Перевернуть массив чисел - reverse
-function reverseArray(arr) {
-    return arr.reverse();
-  }
-  
-  
-  // 4. Разделить строку по пробелам и сохранить каждое слово как элемент массива - split / join
-function splitAndJoinString(str) {
-    return str.split(' ');
-  }
-  
-  
-  // 5. Найти сумму элементов в массиве чисел - reduce / reduceRight
-function sumArray(arr) {
-    return arr.reduce((acc, curr) => acc + curr, 0);
-  }
-  
-  
-  // 6. Напишите функцию camelize(str), которая преобразует строки вида my-short-string в myShortString
-function camelize(str) {
-    return str.split('-').map((word, index) => index === 0 ? word : word[0].toUpperCase() + word.slice(1)).join('');
-  }
 
-  // 7. Функция filterRange
-function filterRange(arr, a, b) {
-    return arr.filter(num => num >= a && num <= b);
+function reverseArray(array) {
+  const reversedArray = [];
+  for (let i = array.length - 1; i >= 0; i--) {
+    reversedArray.push(array[i]);
   }
-  
+  return reversedArray;
+}
 
-  
-  // 8. Функция filterRangeInPlace
-function filterRangeInPlace(arr, a, b) {
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] < a || arr[i] > b) {
-        arr.splice(i, 1);
-        i--; 
-      }
-    }
-  }
-  
+console.log(reverseArray([1, 2, 3, 4, 5])); 
 
-// 9. Сортировка в порядке убывания
-function sortDescending(arr) {
-    return arr.sort((a, b) => b - a);
-  }
-  
 
-  
+function compact(array) {
+  return array.filter((item) => item !== false && item !== undefined && item !== '' && item !== 0 && item !== null);
+}
+
+console.log(compact([0, 1, false, 2, '', 3, null, undefined])); 
