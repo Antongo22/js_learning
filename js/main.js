19218544f43a59da2b85d47dddc6a9fb2714408c
Anton <80984465+Antongo22@users.noreply.github.com>
2024-03-26 11:55:04 +0300

    задния на паре

--- js/main.js
+++ js/main.js
@@ -1,27 +1,112 @@
-function fillArray(value, length) {
-  const newArray = [];
-  for (let i = 0; i < length; i++) {
-    newArray.push(value);
+// 1
+let person = {
+  name: "Anton", 
+  sayHi: function() {
+    console.log("Привет, меня зовут " + this.name + "!");
   }
-  return newArray;
+};
+
+person.sayHi(); 
+
+
+// 2
+function showGlobalThis() {
+  console.log(this);
 }
 
-console.log(fillArray('a', 3)); 
+showGlobalThis();
 
 
-function reverseArray(array) {
-  const reversedArray = [];
-  for (let i = array.length - 1; i >= 0; i--) {
-    reversedArray.push(array[i]);
+// 3
+class Car {
+  constructor(brand) {
+    this.brand = brand;
   }
-  return reversedArray;
+
+  showBrand() {
+    console.log("Марка автомобиля: " + this.brand);
+  }
 }
 
-console.log(reverseArray([1, 2, 3, 4, 5])); 
+let myCar = new Car("Toyota");
 
+myCar.showBrand(); 
 
-function compact(array) {
-  return array.filter((item) => item !== false && item !== undefined && item !== '' && item !== 0 && item !== null);
+// 4
+function Car1(brand) {
+  this.brand = brand;
 }
 
-console.log(compact([0, 1, false, 2, '', 3, null, undefined])); 
+function showBrand() {
+  console.log("Марка автомобиля: " + this.brand);
+}
+
+let myCar1 = new Car1("Toyota");
+showBrand.call(myCar1); 
+
+
+// 5
+const calculator = {
+  add: function(a, b) {
+    return a + b;
+  },
+  subtract: function(a, b) {
+    return a - b;
+  },
+  multiply: function(a, b) {
+    return a * b;
+  },
+  divide: function(a, b) {
+    if (b === 0) {
+      return "Деление на ноль невозможно";
+    }
+    return a / b;
+  }
+};
+
+console.log("Сумма:", calculator.add(5, 3));   
+console.log("Разность:", calculator.subtract(8, 2)); 
+console.log("Произведение:", calculator.multiply(4, 6)); 
+console.log("Частное:", calculator.divide(10, 2)); 
+console.log("Деление на ноль:", calculator.divide(10, 0)); 
+
+// 6
+const user = {
+  name: "John",
+  changeName: function(newName) {
+    this.name = newName;
+  }
+};
+
+console.log("Изначальное имя:", user.name); 
+
+user.changeName("Alice");
+
+console.log("Новое имя:", user.name); 
+
+// 7 
+const person1 = {
+  name: "John",
+  introduce: function() {
+    console.log("Привет, меня зовут " + this.name + "!");
+  }
+};
+
+person1.introduce(); 
+
+// 8
+const timer = {
+  start: function(callback, time) {
+    setTimeout(callback, time);
+  }
+};
+
+function callbackFunction() {
+  console.log("Таймер сработал!");
+}
+
+console.log("Запускаем таймер...");
+timer.start(callbackFunction, 3000); 
+
+
+
